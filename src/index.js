90f1593367ed49731da84d5269bd7f3b00030d06
alexaolalde <alejandra.olalde1817@gmail.com>
2023-07-19 16:29:10 -0600

    Modificando los index para hacer las metricas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+function updateCount() {
+  const text = document.getElementById("text-input").value;
+  const count = text.length;
+  document.getElementById("char-count").textContent = count;
+}
+
