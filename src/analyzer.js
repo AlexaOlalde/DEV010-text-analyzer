911a473b9971d914ee0b24a2a78005807c810e61
alexaolalde <alejandra.olalde1817@gmail.com>
2023-07-27 10:58:18 -0600

    Primer Commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -4,25 +4,30 @@ const analyzer = {
     return words.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCount: (characters) => {
+  getCharacterCount: (text) => {
     const characters = text.length;
     return characters;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (charactersNoSpaces) => {
+  getCharacterCountExcludingSpaces: (text) => {
+    const charactersNoSpaces = text.replace(/\s/g, '').length;
     return charactersNoSpaces;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (wordLengthsAverage) => {
+  getAverageWordLength: (text) => {
+    const wordLengths = words.map(word => word.length);
+    const wordLengthsSum = wordLengths.reduce((acc, len) => acc + len, 0);
+    const wordLengthsAverage = wordLengths.length ? (wordLengthsSum / wordLengths.length).toFixed(2) : 0;
     return wordLengthsAverage;
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
-    const numbers = text.match(/\d+/g) || [].length;
+    const numbers = text.match(/\d+/g) || [];
     return numbers;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberSum: (numbersSum) => {
+  getNumberSum: (text) => {
+    const numbersSum = numbers.reduce((acc, num) => acc + parseInt(num), 0);
     return numbersSum;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
